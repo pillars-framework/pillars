e1001d5aa8aeb984d081729381209c5325eb0876
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-02-19 21:36:49 +0100

    Added main entry point

--- Server/Plugin.cs
+++ Server/Plugin.cs
@@ -1,6 +1,16 @@
 ﻿namespace Pillars;
 
-public sealed class Plugin
+public sealed class Plugin : IPlugin
 {
+    public string Author { get; } = "Pillars Team";
+    public string Name { get; } = "Pillars Framework";
+    public Version Version { get; } = new(1, 0, 0);
 
+    public Plugin()
+    {
+        
+    }
+
+    public void PostLoad() {}
+    public void Shutdown() {}
 }
