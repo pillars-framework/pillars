295a3ceb3b173171892796473294902a096d9ed7
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-02-20 01:43:50 +0100

    Updated globals

--- Server/GlobalUsings.cs
+++ Server/GlobalUsings.cs
@@ -13,6 +13,18 @@ global using Serilog;
 global using Serilog.Events;
 global using Serilog.Settings.Configuration;
 global using ILogger = Serilog.ILogger;
+global using Pillars.Core.Logging;
+global using Pillars.Database.Configuration;
+global using MongoDB.Entities;
+global using Microsoft.Extensions.DependencyInjection;
+global using Pillars.Core.DI.Interfaces;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Pillars.Core.Configuration.Models;
+global using Pillars.Core.DI.Helpers;
+
 
 namespace Pillars;
 
