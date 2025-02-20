14d071fd532769f8f9f6718ba6570a3af2040f00
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-02-20 01:43:35 +0100

    Added mongoDb stuff

---
+++ Server/Database/Configuration/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Pillars.Database.Configuration;
+
+public sealed class DatabaseSettings : ISettings
+{
+    public string Database { get; set; } = default!;
+    public string Host { get; set; } = default!;
+    public int Port { get; set; } = 27017;
+}
---
+++ Server/Database/Controllers/DatabaseController.cs
@@ -0,0 +1,18 @@
+﻿namespace Pillars.Database.Controllers;
+
+[RegisterSingleton(typeof(DatabaseController))]
+public sealed class DatabaseController(ILogger logger, DatabaseSettings settings) : IController
+{
+    private readonly ILogger _logger = logger.ForThisContext();
+    public async Task InitializeAsync()
+    {
+        try
+        {
+            await DB.InitAsync(settings.Database, settings.Host, settings.Port);
+        }
+        catch (Exception e)
+        {
+            _logger.Error(e);
+        }
+    }
+}
