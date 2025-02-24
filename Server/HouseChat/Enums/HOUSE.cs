63fecd90f839f1579ec864a39d292d57866a6df2
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-02-24 19:40:32 +0100

    Add house enmu

---
+++ Server/HouseChat/Enums/HOUSE.cs
@@ -0,0 +1,13 @@
+﻿namespace Pillars.HouseChat.Enums;
+
+/// <summary>
+/// Represents the different houses that a player can belong to.
+/// </summary>
+public enum HOUSE
+{
+	Gryffindor,
+	Hufflepuff,
+	Ravenclaw,
+	Slytherin,
+	Unaffiliated
+}
