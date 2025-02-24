2b175e0a2716008ba42edb2d2626944fc349562a
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-02-24 19:40:15 +0100

    Add global chat

---
+++ Server/GlobalChat/Controllers/GlobalChatController.cs
@@ -0,0 +1,41 @@
+﻿namespace Pillars.GlobalChat.Controllers;
+
+[RegisterSingleton]
+public sealed class GlobalChatController
+{
+	private readonly PlayerController _playerController;
+	private readonly PiChatActor _piChatActor;
+
+	public GlobalChatController(PlayerController pc, ChatEvents ce, PiChatActor pica)
+	{
+		_playerController = pc;
+		_piChatActor = pica;
+		ce.OnChatMessage += OnChatMessage;
+	}
+
+	/// <summary>
+	/// Handles incoming chat messages from players and broadcasts them to all players in the global chat channel.
+	/// </summary>
+	/// <param name="player">The player who sent the chat message.</param>
+	/// <param name="message">The chat message sent by the player.</param>
+	/// <remarks>
+	/// This method checks if the message is not a command (does not start with '/') and if the player is in the global chat channel.
+	/// If both conditions are met, the message is broadcast to all players in the global chat channel, prefixed with the sender's username.
+	/// </remarks>
+	private async Task OnChatMessage(PiPlayer player, string message)
+	{
+		if (message.StartsWith('/') || player.ActiveChatChannel != CHATCHANNEL.GLOBAL) return;
+
+		foreach (var targetPlayer in _playerController.Players.Keys)
+			_piChatActor.SendMessageToPlayer(targetPlayer, $"{player.Username}: " + message);
+
+	}
+
+	/// <summary>
+	/// Handles the "/global" slash command, which switches the player's active chat channel to the global channel.
+	/// </summary>
+	/// <param name="player">The player who executed the command.</param>
+	[SlashCommand("global")]
+	public async Task GlobalCommand(PiPlayer player) => player.ActiveChatChannel = CHATCHANNEL.HOUSE;
+
+}
