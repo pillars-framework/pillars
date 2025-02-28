9d25fa682c15645d84ffdf12792b7a3227d1d8d7
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-02-28 21:16:57 +0100

    Add weather events

---
+++ Server/Weather/Events/WeatherEvents.cs
@@ -0,0 +1,26 @@
+﻿namespace Pillars.Weather.Events;
+
+[RegisterSingleton]
+public sealed class WeatherEvents
+{
+	public delegate Task OnWeatherChangeDelegate(WEATHER weather);
+
+	public event OnWeatherChangeDelegate? OnWeatherChange;
+
+	public void WeatherChange(WEATHER weather)
+		=> OnWeatherChange?.Invoke(weather);
+
+	public delegate Task OnSeasonChangeDelegate(SEASON season);
+
+	public event OnSeasonChangeDelegate? OnSeasonChange;
+
+	public void SeasonChange(SEASON season)
+		=> OnSeasonChange?.Invoke(season);
+
+	public delegate Task OnTimeChangeDelegate(DateTime dateTime);
+
+	public event OnTimeChangeDelegate? OnTimeChange;
+
+	public void TimeChange(DateTime dateTime)
+		=> OnTimeChange?.Invoke(dateTime);
+}
