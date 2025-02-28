20327891b0595e5ef90d68175267450ec0fce6ab
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-02-28 21:15:44 +0100

    Add season and weather enums

---
+++ Server/Chat/Data/WEATHER.cs
@@ -0,0 +1,10 @@
+﻿// ReSharper disable InconsistentNaming
+namespace Pillars.Chat.Data;
+
+public enum WEATHER
+{
+	Clear,
+	Misty_01,
+	Stormy_01,
+	StormyLarge_01
+}
---
+++ Server/Weather/Data/SEASON.cs
@@ -0,0 +1,10 @@
+﻿namespace Pillars.Weather.Data;
+
+public enum SEASON
+{
+	Invalid = 0,
+	Fall = 1,
+	Winter = 2,
+	Spring = 3,
+	Summer = 4,
+}
