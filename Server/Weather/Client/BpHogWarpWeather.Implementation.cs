83800ceb09995d6528df14e98e96025bb2438b57
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-02-28 21:16:26 +0100

    Add client / server event implementation

---
+++ Server/Weather/Client/BpHogWarpWeather.Implementation.cs
@@ -0,0 +1,15 @@
+﻿namespace HogWarp.Replicated;
+
+public sealed partial class BpHogWarpWeather
+{
+	public required WeatherActor ParentActor { get; set; }
+
+	public partial void SendWeather(Player player, string weather)
+		=> ParentActor.WeatherChanged(weather);
+
+	public partial void SendSeason(Player player, int season)
+		=> ParentActor.SeasonChanged(season);
+
+	public partial void SendTime(Player player, int hour, int minute, int second)
+		=> ParentActor.TimeChanged(hour, minute, second);
+}
