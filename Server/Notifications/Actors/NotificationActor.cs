c3338e8608ad6b50be7fd3c4d6b719d2310be076
PiepMatz <rappersinfan@gmail.com>
2025-03-04 10:59:42 +0100

    feat: Notification: Start of notification module

---
+++ Server/Notifications/Actors/NotificationActor.cs
@@ -0,0 +1,8 @@
+﻿namespace Pillars.Notifications.Actors;
+
+[RegisterSingleton]
+public sealed class NotificationActor : PiActor<BpNotification>
+{
+	public async Task Test(PiPlayer player, string title, string text) =>
+		_worldActor.TriggerNotification(player.Native, title, text, 5f);
+}
---
+++ Server/Notifications/Controllers/NotificationController.cs
@@ -0,0 +1,19 @@
+﻿namespace Pillars.Notifications.Controllers;
+
+using Actors;
+
+[RegisterSingleton]
+public sealed class NotificationController(ILogger l, NotificationActor actor)
+{
+	private readonly ILogger _logger = l.ForThisContext();
+
+	[SlashCommand("notify")]
+	private async Task TestNotification(PiPlayer player, string[] args)
+	{
+		_logger.Information("Testing notification with args: {a}", JsonSerializer.Serialize(args));
+		if (args.Length <= 2) return;
+		string title = args[0];
+		string message = string.Join(" ", args[1..]);
+		actor.Test(player, title, message);
+	}
+}
