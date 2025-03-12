180b71872734ba6875bcd79f180f79e0e940c04f
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-03-12 18:11:13 +0100

    Add dialog actor

---
+++ Server/Dialog/Actors/DialogActor.cs
@@ -0,0 +1,28 @@
+﻿namespace Pillars.Dialog.Actors;
+
+[RegisterSingleton]
+public sealed class DialogActor : PiActor<BpPillarsDialog>
+{
+	private readonly ConcurrentDictionary<string, TaskCompletionSource<DIALOGRESULT>> _pendingDialogs = new();
+
+	public DialogActor()
+	{
+		_worldActor.ParentActor = this;
+	}
+
+	public void DialogResultReceived(string dialogId, DIALOGRESULT dialogResult)
+	{
+		if (_pendingDialogs.TryRemove(dialogId, out var request))
+			request.SetResult(dialogResult);
+	}
+
+	public async Task<DIALOGRESULT> ShowDialogAsync(PiPlayer player, string title, string message)
+	{
+		var dialogId = Guid.NewGuid().ToString();
+		var tcs = new TaskCompletionSource<DIALOGRESULT>();
+		_pendingDialogs[dialogId] = tcs;
+		_worldActor.ShowDialog(player.Native, dialogId, title, message);
+		return await tcs.Task;
+	}
+
+}
