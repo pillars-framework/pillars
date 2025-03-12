0052ea15490a3caf4552ee6034f0193e10224820
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-03-12 18:11:04 +0100

    Add client implementation

---
+++ Server/Dialog/Client/BpPillarsDialog.Implementation.cs
@@ -0,0 +1,7 @@
+﻿namespace HogWarp.Replicated;
+
+public sealed partial class BpPillarsDialog
+{
+	public required DialogActor ParentActor { get; set; }
+	public partial void DialogResult(NativePlayer player, string dialogId, int result) => ParentActor.DialogResultReceived( dialogId, (DIALOGRESULT)result);
+}
